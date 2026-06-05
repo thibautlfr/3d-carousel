25c4fd70b6ef8b2e6639b4c38bf37eff5c43c422
thibautlfr <thibaut.lfr@gmail.com>
2026-06-05 13:23:46 +0200

    feat: update base path in Vite configuration for production environment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import glsl from "vite-plugin-glsl"
 
 export default {
-  base: "/3d-carousel/",
+  base: process.env.NODE_ENV === "production" ? "/3d-carousel/" : "/",
   plugins: [
     glsl()
   ]
